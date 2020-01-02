432f635e000df8fc1ab878e6fbc7807680b1b2e1
wackon <jeac86@gmail.com>
2020-01-02 13:51:05 -0500

    añadimos detector de errores y creamos un component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
+// function Hello (props){
+//   return <h2>{props.title}</h2>
+
+//  const Hello = (props) => <h2>{props.title}</h2>
+
+class Hello extends Component{
+  render(){
+    return <h2>{this.props.title}</h2>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <Hello title='Hello from props'/>
+      </div>
     </div>
   );
 }
